92b7a05f74348268a52062b8cb99e0fade66c676
KMolina <kevenmolina@gmail.com>
2019-05-24 11:33:47 -0500

    connected to DB

--- models/user.js
+++ models/user.js
@@ -2,20 +2,14 @@ const mongoose= require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
-    name:{
-        type:String
-    },
-    email:{
-        type:String
-    },
-    dog:
-    {
-        type:Array
-    },
+    name:{type:String},
+    email:{type:String},
+    dog:{type:Array},
     Walker:{
         type:Boolean,
         default:false
-    }
+    },
+    googleId:String
     });
 
 module.exports= mongoose.model(User, userSchema)
